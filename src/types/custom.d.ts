a7f1396ae213406e321b37796f008766ccea6ca0
Sabrina Travasso Ferreira <53414857+satravasso@users.noreply.github.com>
2025-11-30 13:57:54 -0300

    refactor: remove redundant CSS module type declarations and consolidate them into custom.d.ts

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -9,8 +9,12 @@ declare module "*.scss" {
   const content: Record<string, string>
   export default content
 }
-
 declare module "*.module.scss" {
-  const content: Record<string, string>
-  export default content
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string }
+  export default classes
 }
